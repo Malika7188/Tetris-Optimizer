5270fca9d36405ff0dd1f962d048c3456ca01f54
masman <asmanoden@gmail.com>
2024-10-08 21:59:55 +0300

    chore: added file reader function

---
+++ functions/filereader.go
@@ -0,0 +1,49 @@
+package functions
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ReadFile() [][]string {
+	if len(os.Args) != 2 {
+		fmt.Println("error")
+		os.Exit(0)
+	}
+
+	args := os.Args[1]
+
+	filename, err := os.ReadFile(args)
+	if err != nil {
+		fmt.Printf("error %v", err)
+		os.Exit(0)
+	}
+	tetrominos := [][]string{}
+	eachTetro := []string{}
+	var letter rune = 'A'
+
+	file := strings.Split(string(filename), "\n")
+
+	for _, str := range file {
+		if str != "" {
+			s := ""
+			for _, c := range str {
+				if c == '#' {
+					s += string(letter)
+				} else if c == '.' {
+					s += "."
+				} else {
+					fmt.Println("error")
+					os.Exit(0)
+				}
+			}
+			eachTetro = append(eachTetro, s)
+		} else {
+			tetrominos = append(tetrominos, eachTetro)
+			eachTetro = []string{}
+			letter++
+		}
+	}
+	return tetrominos
+}
